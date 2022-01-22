668c9cb85b3f2b69d06b8e1037ce7396e97e2665
Fred <fredlefevre@outlook.com>
2022-01-22 13:53:11 -0500

    Configured eslint, installed react-native-paper and implemented search bar

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView>
-        <View style={{ padding: 16, backgroundColor: "green" }}>
-          <Text>search</Text>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar placeholder="Search restaurants" />
         </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+        <View style={styles.list}>
           <Text>list</Text>
         </View>
       </SafeAreaView>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
